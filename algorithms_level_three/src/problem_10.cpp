0f960870986545700cf87c63f35f367ecd3f652a
Zain Orwa <orwazain3@gmail.com>
2025-08-25 17:47:48 +0200

    problem_10 added

---
+++ algorithms_level_three/src/problem_10.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+short  ReadYear(){
+   short Year = 1971;
+   cout << "\nPlease enter a year: ";
+   cin >> Year;
+   return (Year);
+}
+
+short ReadMonth(){
+    short Month = 1;
+    cout << "\nPlease enter a Month: ";
+    cin >> Month;
+    return (Month);
+}
+
+
+
+short ReadDay(){
+    short DayOfMonth = 0;
+    cout << "\nPlease enter a day: ";
+    cin >> DayOfMonth;
+    return (DayOfMonth);
+}
+
+bool isLeapYear(short Year){
+    return ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0));
+}
+
+short NumberOfDaysInMonth(short Month, short Year){
+    if (Month < 1 || Month > 12)
+        return (0);
+
+    short NumberOfDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return ((Month == 2) ? (isLeapYear(Year)) ? 29 : 28 : (NumberOfDays[Month - 1]));
+
+}
+
+
+short NumberOfDaysFromBeginningOfTheYear(short Day, short Month, short Year){
+
+    short TotalDays = 0;
+
+    for (short i = 0; i < Month - 1; i++){
+        TotalDays += NumberOfDaysInMonth(i, Year);
+    }
+    
+    TotalDays += Day;
+
+    return (TotalDays); 
+}
+
+
+int main(){
+    short Day   = ReadDay();
+    short Month = ReadMonth();
+    short Year  = ReadYear();
+
+    cout << "\nNumber of Days from the beginning of the year is: "
+         << NumberOfDaysFromBeginningOfTheYear(Day, Month, Year);
+    cout << "\n\n";
+
+
+    return (0);
+}
